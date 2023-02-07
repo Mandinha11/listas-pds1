e701ab93a2ef621d884f95581e66ebe1359a6394
Felps11 <111304063+Mandinha11@users.noreply.github.com>
2023-02-07 08:59:06 -0300

    Felps11

---
+++ Aula1/src/modelo/Funcionario.java
@@ -0,0 +1,16 @@
+package modelo;
+
+public class Funcionario extends pessoa{
+
+	private Long cpf;
+
+	public Long getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(Long cpf) {
+		this.cpf = cpf;
+	}
+	
+	
+}
---
+++ Aula1/src/visao/janelaCadastro.java
@@ -0,0 +1,52 @@
+package visao;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+
+public class janelaCadastro extends JFrame {
+
+	private JPanel contentPane;
+	private JTextField texto;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					janelaCadastro frame = new janelaCadastro();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public janelaCadastro() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		
+		JLabel lblTexto = new JLabel("Nome pessoa");
+		contentPane.add(lblTexto);
+		
+		texto = new JTextField();
+		texto.setText("teste");
+		contentPane.add(texto);
+		texto.setColumns(10);
+	}
+
+}
